81b8dfa37bb51495c3b129b2df2bad6df30011a5
Macbook Pro <macbookpro@MacBook-Pro-Macbook.local>
2025-07-24 00:31:04 +0300

    modified:   main.py

--- guess_the_number/main.py
+++ guess_the_number/main.py
@@ -3,6 +3,7 @@ import random
 random_number = random.randint(1, 100)
 count = 0
 
+
 def is_valid(num):
     if num.isdigit():
         num = int(num)
@@ -13,23 +14,27 @@ def is_valid(num):
     else:
         return False
 
-print('Добро пожаловать в числовую угадайку')
+
+print("Добро пожаловать в числовую угадайку")
 while True:
     user_number = input("Введите целое число от 1 до 100: ")
 
     if not is_valid(user_number):
         print("Введите целое число!\n")
         continue
+
     user_number = int(user_number)
+    count += 1
+
     if random_number < user_number:
         print()
-        print('Слишком много, попробуйте еще раз')
+        print("Слишком много, попробуйте еще раз")
     elif random_number > user_number:
         print()
         print("Слишком мало, попробуйте еще раз")
     else:
         print()
-        print(f"Вы угадали за {count} ходов, поздравляем!")
         break
-    count += 1
-print('Спасибо, что играли в числовую угадайку.')
+
+print(f"Вы угадали за {count} ходов, поздравляем!")
+print("Спасибо, что играли в числовую угадайку.")
